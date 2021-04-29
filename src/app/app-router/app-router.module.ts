fa19983c0a011630171d13fac06c7bf8c67cb74b
josdugan <josdugan@gmail.com>
2021-04-29 14:15:14 -0400

    Add home route

--- src/app/app-router/app-router.module.ts
+++ src/app/app-router/app-router.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from '../app.component';
 import { ShoppingListComponent } from '../shopping-list/shopping-list.component';
 import { RecipesComponent } from '../recipes/recipes.component';
 
 const appRoutes : Routes = [
+  { path: '', redirectTo: '/recipes' },
   { path: 'shopping-list', component: ShoppingListComponent },
   { path: 'recipes', component: RecipesComponent }
 ];

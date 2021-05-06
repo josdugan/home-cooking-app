05a9fc5ae99cec24c13828a7e70cb899e3fcedf2
josdugan <josdugan@gmail.com>
2021-05-06 16:50:10 -0400

    Add auto login and auto logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
-import { Component, Output } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  constructor() {
+  constructor(private auth: AuthService) {
 
+  }
+
+  ngOnInit() {
+    this.auth.autoLogin();
   }
   
 }

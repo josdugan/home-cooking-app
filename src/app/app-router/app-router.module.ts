b04d20c1cfbb1bb9a16c2ac51cf4ef8fbd4947c1
josdugan <josdugan@gmail.com>
2021-05-11 15:17:07 -0400

    Refactor and create recipes module

--- src/app/app-router/app-router.module.ts
+++ src/app/app-router/app-router.module.ts
@@ -1,25 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ShoppingListComponent } from '../shopping-list/shopping-list.component';
-import { RecipesComponent } from '../recipes/recipes.component';
-import { RecipeDetailsComponent } from '../recipes/recipe-details/recipe-details.component';
-import { RecipeStartComponent } from '../recipes/recipe-start/recipe-start.component';
-import { RecipeEditComponent } from '../recipes/recipe-edit/recipe-edit.component';
-import { RecipeResolverService } from '../recipes/recipe-resolver.service';
 import { AuthComponent } from '../auth/auth.component';
-import { AuthGuard } from '../auth/auth.guard';
+import { ShoppingListComponent } from '../shopping-list/shopping-list.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
   { path: 'shopping-list', component: ShoppingListComponent },
-  {
-    path: 'recipes', component: RecipesComponent, canActivate: [AuthGuard], children: [
-      { path: '', component: RecipeStartComponent },
-      { path: 'new', component: RecipeEditComponent },
-      { path: ':id', component: RecipeDetailsComponent, resolve: [RecipeResolverService] },
-      { path: ':id/edit', component: RecipeEditComponent, resolve: [RecipeResolverService] },
-    ]
-  },
   { path: 'auth', component: AuthComponent }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,37 +2,26 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { AlertComponent } from './alert/alert.component';
 import { AppRouterModule } from './app-router/app-router.module';
 import { AppComponent } from './app.component';
+import { AuthInterceptorService } from './auth/auth-interceptor.service';
+import { AuthComponent } from './auth/auth.component';
 import { HeaderComponent } from './header/header.component';
-import { RecipeDetailsComponent } from './recipes/recipe-details/recipe-details.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipesComponent } from './recipes/recipes.component';
+import { RecipesModule } from './recipes/recipes.module';
 import { DropdownDirective } from './shared/dropdown.directive';
+import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { AuthComponent } from './auth/auth.component';
-import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
-import { AuthInterceptorService } from './auth/auth-interceptor.service';
-import { AlertComponent } from './alert/alert.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeItemComponent,
-    RecipeDetailsComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
     DropdownDirective,
-    RecipeStartComponent,
-    RecipeEditComponent,
     AuthComponent,
     LoadingSpinnerComponent,
     AlertComponent
@@ -43,6 +32,7 @@ import { AlertComponent } from './alert/alert.component';
     ReactiveFormsModule,
     HttpClientModule,
     AppRouterModule,
+    RecipesModule
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true }
